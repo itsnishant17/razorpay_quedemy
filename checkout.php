f6fcd40c57097c79a2fb92276da3c07e05ea233c
Nishant Kumar <work.nishantkumar@gmail.com>
2023-03-23 17:15:11 +0530

    3rd commit

--- checkout.php
+++ checkout.php
@@ -18,10 +18,11 @@
                             <label for="email" id="fname"><i class="fa fa-envelope"></i>Email</label>
                             <input type="text" id="email" name="email" placeholder="nishant@example.com">
                             <input type="hidden" value="<?php echo 'OID'.rand(10,100);?>" name="orderid">
+                            <input type="hidden" value="<?php echo 1;?>" name="amount">
                         </div>
                         
                     </div>
-                    <input type="sumbit" value="pay now" class="btn">
+                    <input type="submit" value="Pay Now" class="btn">
                 </form>
             </div>
         </div>
