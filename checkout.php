9793181a8287890727147b6293f69be1413e5c91
Nishant Kumar <work.nishantkumar@gmail.com>
2023-03-23 17:03:30 +0530

    2nd commit

---
+++ checkout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout</title>
+</head>
+<body>
+    <div class="row" style="padding: 100px 300px;">
+        <div class="col-50">
+            <div class="container">
+                <form action="payscript.php" method="post" style="padding: 25px;">
+                    <div class="row">
+                        <div class="col-25">
+                            <h3 style="text-align: center;margin: 20px 10px;font-family: lato;">Checkout Form</h3>
+                            <label for="fname"><i class="fa fa-user"></i>Full Name</label>
+                            <input type="text" id="fname" name="name" placeholder="Nishant Kumar">
+                            <label for="email" id="fname"><i class="fa fa-envelope"></i>Email</label>
+                            <input type="text" id="email" name="email" placeholder="nishant@example.com">
+                            <input type="hidden" value="<?php echo 'OID'.rand(10,100);?>" name="orderid">
+                        </div>
+                        
+                    </div>
+                    <input type="sumbit" value="pay now" class="btn">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
